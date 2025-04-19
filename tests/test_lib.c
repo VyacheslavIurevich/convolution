fa2034f45a15ddb8251c954678f5748fc29817dd
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-04-19 10:55:19 +0300

    chore: add tests and Makefile
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

---
+++ tests/test_lib.c
@@ -0,0 +1,17 @@
+#include "../src/lib.h"
+#include <cmocka.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+
+static void test_function_a_returns_5(void **state) {
+  (void)state;
+  assert_int_equal(a(), 5);
+}
+
+int main(void) {
+  const struct CMUnitTest tests[] = {
+      cmocka_unit_test(test_function_a_returns_5),
+  };
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
