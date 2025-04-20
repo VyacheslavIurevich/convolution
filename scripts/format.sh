d1343e7dc974a74129a23e6b0a0869022abfd5af
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-04-20 22:00:38 +0300

    fix: fix format scripts
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 echo "Format the code"
-find . -type f -name "*.c" -o -name "*.h" | xargs -r clang-format -style=llvm -i
+find . -type f \( -name "*.c" -o -name "*.h" \) -print0 | xargs -r clang-format -style=llvm -i
 echo "OK"
