e5df1b79942eb83607b1135424ce4f72a212bf6d
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-06-22 20:38:54 +0300

    fix: make checkmake.sh silent
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/ci/checkmake.sh
+++ scripts/ci/checkmake.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 echo "Check makefile"
-go run github.com/mrtazz/checkmake/cmd/checkmake@latest Makefile
+go run github.com/mrtazz/checkmake/cmd/checkmake@latest Makefile 2>/dev/null
 echo "OK"
