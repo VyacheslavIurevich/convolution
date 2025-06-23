baa0cbeaff4762530cd18e5e6e3eefb4f988def9
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-06-23 21:17:26 +0300

    add rm -f flag
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- scripts/ci/lint.sh
+++ scripts/ci/lint.sh
@@ -4,5 +4,5 @@ make clean
 bear -- make build
 find . -type f \( -name "*.c" -o -name "*.h" \) -print0 | xargs -r -0 clang-tidy -p
 make clean
-rm compile_commands.json
+rm -f compile_commands.json
 echo "OK"
