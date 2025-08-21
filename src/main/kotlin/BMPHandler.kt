20a333ab6e8988c938796f2bc5264cc804ed9767
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-08-21 19:38:28 +0300

    seq: add error messages to BMPHandler
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- src/main/kotlin/BMPHandler.kt
+++ src/main/kotlin/BMPHandler.kt
@@ -6,15 +6,15 @@ import org.bytedeco.opencv.global.opencv_imgcodecs.*
 
 class BMPHandler {
     fun readBMP(input: String): Mat {
-        require(input.endsWith(".bmp"))
+        require(input.endsWith(".bmp")) { "Input file must be .bmp, got $input" }
         val image: Mat? = imread(input)
-        require(image != null)
+        require(image != null) { "Image must be successfully read" }
         return image
     }
 
     fun writeBMP(output: String, image: Mat) {
-        require(output.endsWith(".bmp"))
+        require(output.endsWith(".bmp")) { "Output file must be .bmp, got $output" }
         val result = imwrite(output, image)
-        require(result)
+        require(result) { "Write must be successful" }
     }
 }
