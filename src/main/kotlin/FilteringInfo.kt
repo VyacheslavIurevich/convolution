29a83ba6ac9d4ee23a10f0487adc6fe35babf74a
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-08-30 12:02:27 +0300

    seq: update FilteringInfo
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- src/main/kotlin/FilteringInfo.kt
+++ src/main/kotlin/FilteringInfo.kt
@@ -3,12 +3,12 @@ package org.example
 class FilteringInfo(
     val filter: Array<DoubleArray>, val factor: Double, val bias: Double
 ) {
+    val rows = filter.size
+    val cols = filter.first().size
+
     init {
         require(filter.isNotEmpty()) { "Filter matrix must be non-empty" }
         require(filter.all { it.isNotEmpty() }) { "All filter rows must be non-empty" }
-
-        val rows = filter.size
-        val cols = filter.first().size
 
         require(rows % 2 == 1) { "Filter must have an odd number of rows, got $rows" }
         require(cols % 2 == 1) { "Filter must have an odd number of columns, got $cols" }
